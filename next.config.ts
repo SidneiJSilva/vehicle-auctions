5911085b1313e7c1f3431e5a00d2903271d7ccb8
sidnei silva <tjsidnei@gmail.com>
2025-04-15 15:33:18 +0100

    fix build to prd

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  reactStrictMode: false
+const nextConfig = {
+  reactStrictMode: false,
   // Disable Strict Mode only for testing in
   // specific cases (e.g. for clean up
   // localstorage during development)
+  experimental: {
+    turbo: false
+  }
+  // Disables Turbopack for production builds.
+  // Turbopack is currently only stable for development (next dev),
+  // but incompatible with next build + next start in some setups.
+  // Setting turbo: false forces Next.js to use Webpack instead.
 }
 
 export default nextConfig
--- src/app/vehicle/[id]/page.tsx
+++ src/app/vehicle/[id]/page.tsx
@@ -1,7 +1,12 @@
 import VehicleDetails from '@/components/vehicle/details/VehicleDetails'
 
-export default async function Vehicle({ params }: { params: { id: string } }) {
+export default async function VehiclePage({
+  params
+}: {
+  params: Promise<{ id: string }>
+}) {
   const { id } = await params
+
   // Dynamic APIs are Asynchronous
   // In Next 15, these APIs have been made asynchronous.
   // If the warning occurred on the Server (e.g. a route handler,
